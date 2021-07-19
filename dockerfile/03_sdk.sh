bfe851bcf709f24a15ec4f2384df04cb6b12dff0
volcano <volcano_dr@163.com>
2021-07-19 18:06:40 +0800

    docker: Update Dockfile (intel sgx sdk 2.14, DCAP 1.11)

--- dockerfile/03_sdk.sh
+++ dockerfile/03_sdk.sh
@@ -10,10 +10,10 @@ else
     cd /root && \
     git clone --recursive https://github.com/intel/linux-sgx && \
     cd linux-sgx && \
-    git checkout sgx_2.13 && \
+    git checkout sgx_2.14 && \
     ./download_prebuilt.sh && \
     make -j "$(nproc)" sdk_install_pkg && \
-    echo -e 'no\n/opt' | ./linux/installer/bin/sgx_linux_x64_sdk_2.13.100.4.bin && \
+    echo -e 'no\n/opt' | ./linux/installer/bin/sgx_linux_x64_sdk_2.14.100.2.bin && \
     echo 'source /opt/sgxsdk/environment' >> /root/.bashrc && \
     cd /root && \
     rm -rf /root/linux-sgx
